9a96b55ef5a94d76ee5a7f568ea10ad722d22224
LZH <233748863@qq.com>
2026-06-11 13:51:27 +0800

    新增: 完成所有页面迁移 - 达成 153+ 目标

---
+++ cloudflow-frontend-vue/src/pages/knowledge/TrainingPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { BookOpen } from 'lucide-vue-next'
+import { Panel } from '@/components/common'
+</script>
+
+<template>
+  <Panel title="Training">
+    <template #icon><BookOpen class="h-5 w-5" /></template>
+    <div class="text-center text-gray-500 py-8">暂无数据</div>
+  </Panel>
+</template>
